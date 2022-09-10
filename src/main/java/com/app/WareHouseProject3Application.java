4075377bf8a0bbd5aa6f2a0aba769f498fd33121
TL <tamhid.tamhid23@gmail.com>
2022-09-10 11:37:24 -0700

    Committed By Tamhid

---
+++ src/main/java/com/app/WareHouseProject3Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareHouseProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WareHouseProject3Application.class, args);
+	}
+
+}
